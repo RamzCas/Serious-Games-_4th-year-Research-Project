8eb45e0085f934543376fcf42f441970b9881bd6
RamzCas <2749414@students.wits.ac.za>
2026-03-31 11:57:01 +0200

    work on the script

---
+++ Serious Games/Assets/Scipts/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float Speed;
+    public float Rotation;
+    public Rigidbody2D rb;
+    private Vector2 MoveInput;
+
+    [Header("Interaction")]
+    public float InteractRange;
+
+    PlayerControler Controls;
+
+    private void Awake()
+    {
+        Controls = new PlayerControler();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        Controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        Controls.Disable();
+    }
+
+    private void Update()
+    {
+        MoveInput = Controls.Player.Movement.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = MoveInput * Speed;
+    }
+}
